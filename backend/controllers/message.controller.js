11d562284dd2c9d8848f0f3389c38122f62f732d
Atharva Raju Magre <atharvamagre12@gmail.com>
2024-08-31 21:40:19 +0530

    finalcommit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from '../models/conversation.model.js'
 import Message from '../models/message.model.js'
+import { getReceiverSocketId } from '../socket/socket.js';
+import { io } from '../socket/socket.js';
 
 
 export const sendMessage = async (req, res) => {
@@ -26,10 +28,40 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
         conversation.messages.push(newMessage._id);
     }
+    // await conversation.save();
+		// await newMessage.save();
+    await Promise.all([conversation.save(), newMessage.save()]);
 
+    const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
+    res.status(201).json(newMessage);
 
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+		const messages = conversation.messages;
+
+    res.status(200).json(messages);
+
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,30 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.rotes.js"
-//import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app=express();
+import { app, server } from "./socket/socket.js";
+
 //const PORT=process.env.PORT;
 
 dotenv.config();
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-//app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(process.env.PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+server.listen(process.env.PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${process.env.PORT}`);
 });
