e3dd567f04eacf2ebf0a111688215e468b4855ca
Atharva Raju Magre <atharvamagre12@gmail.com>
2024-08-31 22:33:39 +0530

    deploy1

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://main--glittering-tarsier-28fa78.netlify.app", {
 				query: {
 					userId: authUser._id,
 				},
